b6692402aa175cbc427f97f62cbece3137bd309d
orojiben <orojiben@gmail.com>
2015-04-15 16:29:18 +0700

    test

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.sockets.on('connection', function(socket){
 	  //console.log(data.messages);
       io.sockets.emit('receive_messages_all', { name:data.name,messages:data.messages,
 		img:data.img,color:data.color,color_bg:data.color_bg,time:data.time}); //SV ส่งให้ทุกคน
-		buffer_message.push([data.idu,data.messages,data.color,data.color_bg,data.time]);
+		buffer_message.push([data.idu,data.messages,data.color,data.color_bg,data.time_db]);
 	  //socket.broadcast.emit('hello', { value: data.value }); //SV ส่งให้ทุกคนยกเว้นตัวเอง
 	  //sockets.emit('hello', { value: data.value }); //SV ส่งไห้ตัวเองเท่านั้น
   });
