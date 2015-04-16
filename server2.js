7eac06456a4122d9d9a8471b3815180706115d11
orojiben <orojiben@gmail.com>
2015-04-17 01:26:03 +0700

    test

--- server2.js
+++ server2.js
@@ -1,17 +1,27 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+server = require('http').Server();
+var socketIO = require('socket.io');
+var io = socketIO.listen(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+var buffer_message = new Array();
+
+io.sockets.on('connection', function(socket){
+  console.log("connect");
+  socket.on('send_messages', function(data){
+      //console.log("hello : " + data.value);
+	  //console.log(data.messages);
+      io.sockets.emit('receive_messages_all', { name:data.name,messages:data.messages,
+		img:data.img,color:data.color,color_bg:data.color_bg,time:data.time}); //SV ส่งให้ทุกคน
+		buffer_message.push([data.idu,data.messages,data.color,data.color_bg,data.time_db]);
+	  //socket.broadcast.emit('hello', { value: data.value }); //SV ส่งให้ทุกคนยกเว้นตัวเอง
+	  //sockets.emit('hello', { value: data.value }); //SV ส่งไห้ตัวเองเท่านั้น
   });
+  socket.on('disconnect', function(){
+      console.log("disconnect");
+  });
+  get_messages_connect(socket);
+  //io.sockets.emit('hello', { value: "welcome" });
 });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(8080, function(){
+  console.log('listening on *:8080');
 });
