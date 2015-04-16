7d3830ac60576bf4a6f455b030add341023d5295
orojiben <orojiben@gmail.com>
2015-04-17 02:15:13 +0700

    t
    t

--- server2.js
+++ server2.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
-var socketIO = require('socket.io');
-var io = socketIO.listen(app);
-// New call to compress content
-app.use(express.compress());
+var http = require('http'), io = require('socket.io');
 
-app.use(express.static(__dirname + '/public'));
+// Start the server at port 8866
+var server = http.createServer(function(req, res){
 
-app.listen(process.env.PORT || 3000);
+        // Send HTML headers and message
+        res.writeHead(200,{ 'Content-Type': 'text/html' });
+        res.end('<h1>Hello Socket Lover!</h1>');
+});
+server.listen(8866);
+
+// Create a Socket.IO instance, passing it our server
+var socket = io.listen(server);
